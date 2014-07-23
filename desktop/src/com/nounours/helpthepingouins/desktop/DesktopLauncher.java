9c0af8005955d712118c10015e3edfede166e72f
nounoursheureux <schtroumps31@gmail.com>
2014-07-23 17:45:58 +0200

    Initial commit

---
+++ desktop/src/com/nounours/helpthepingouins/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nounours.helpthepingouins.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nounours.helpthepingouins.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
