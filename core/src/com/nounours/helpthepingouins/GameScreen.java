c70979ee8fae26feb27c9cd49737700959c445e2
nounoursheureux <schtroumps31@gmail.com>
2014-07-23 17:56:08 +0200

    Sauvegarde

---
+++ core/src/com/nounours/helpthepingouins/GameScreen.java
@@ -0,0 +1,57 @@
+package com.nounours.helpthepingouins;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen implements Screen {
+	
+	MyGdxGame game;
+	SpriteBatch batch;
+
+	GameScreen(MyGdxGame game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
